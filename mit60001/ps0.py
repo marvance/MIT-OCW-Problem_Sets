bcc705de0254b046fcb77d4b3b638d178b1ec37c
jennvance <vancemargot@gmail.com>
2018-12-02 20:43:55 -0500

    PS 0

---
+++ mit60001/ps0.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  2 17:53:05 2018
+
+@author: vance
+"""
+
+
+import numpy
+
+
+x = int(input("Enter number x"))
+
+y = int(input("Enter number y"))
+
+result = x**y
+
+print(result)
+
+xlog = numpy.log2(x)
+
+print(xlog)
